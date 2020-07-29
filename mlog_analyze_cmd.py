a640722573b8110fae7ba3a1cc780f85529fdfcd
Haiyun.Li <38989743+HaiyunLi@users.noreply.github.com>
2020-07-29 14:26:51 +0800

    add -s -nod -not -nom -nof option

--- mlog_analyze_cmd.py
+++ mlog_analyze_cmd.py
@@ -79,6 +79,7 @@ def loop_process(arg):
 			break
 
 	print("\n------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------")
+	file.close()
 	return
 
 def dma_process(arg):
@@ -86,11 +87,21 @@ def dma_process(arg):
 	pr_flag = False
 	idx = 1
 	check_count = 0
+	slot_list = []
+	slot_pr = False
+	nodata_flag = False
 	
 	while idx < len(arg):
 		if arg[idx] == "-n":
 			check_count = int(arg[idx+1])
 			idx = idx + 2
+		elif arg[idx] == "-s":
+			slot_list = arg[idx+1].split("&")
+			slot_pr = True
+			idx = idx + 2
+		elif arg[idx] == "-nod":
+			nodata_flag = True
+			idx = idx + 1
 		else:
 			print("ERROR:parameter error !!!")
 			return
@@ -112,7 +123,6 @@ def dma_process(arg):
 		if len(field) != 6:
 			continue
 		if field[1] == "0" and field[4] in [re_condition]:
-			print("\n\n-------------- FhTx",end="")
 			pr_flag = True
 			line_cnt = 0
 			continue
@@ -122,7 +132,14 @@ def dma_process(arg):
 
 		if pr_flag == True:
 			if field[1] == "1":
-				print(" SlotIdx:%5s" % field[3],end="")
+				if slot_pr == True:
+					if field[3] in slot_list:
+						print("\n-------------- FhTx SlotIdx:%5s" % field[3],end="")
+					else:
+						pr_flag = False
+						continue
+				else:
+					print("\n\n-------------- FhTx SlotIdx:%5s" % field[3],end="")
 				continue
 			elif field[1] == "2":
 				print(" Sym:%2s" % field[3],end="")
@@ -130,13 +147,14 @@ def dma_process(arg):
 			elif field[1] == "3":
 				print(" Ant:%s ------------------------------------------------" % field[3])
 				continue
-
-			print("%02x %02x %02x %02x " % ((int(field[3]))&0xff,(int(field[3]) >> 8)&0xff,(int(field[3]) >> 16)&0xff,(int(field[3]) >> 24)&0xff),end="")
-			line_cnt = line_cnt + 4
-			if line_cnt % 32 == 0:
-				print("")
+			if nodata_flag == False:
+				print("%02x %02x %02x %02x " % ((int(field[3]))&0xff,(int(field[3]) >> 8)&0xff,(int(field[3]) >> 16)&0xff,(int(field[3]) >> 24)&0xff),end="")
+				line_cnt = line_cnt + 4
+				if line_cnt % 32 == 0:
+					print("")
 
 	print("")
+	file.close()
 	return
 
 def get_process(arg):
@@ -149,6 +167,10 @@ def get_process(arg):
 	interval_time = 0
 	ticks_cur = 0
 	ticks_prv = 0
+	pr_nomagic = False
+	pr_nofunc = False
+	pr_noticks = False
+	pr_verbose = False
 	
 	while idx < len(arg):
 		if arg[idx] == "-m":
@@ -160,6 +182,18 @@ def get_process(arg):
 		elif arg[idx] == "-t":
 			idx = idx + 1
 			interval_flag = True
+		elif arg[idx] == "-nof":
+			idx = idx + 1
+			pr_nofunc = True
+		elif arg[idx] == "-nom":
+			idx = idx + 1
+			pr_nomagic = True
+		elif arg[idx] == "-not":
+			idx = idx + 1
+			pr_noticks = True
+		elif arg[idx] == "-v":
+			idx = idx + 1
+			pr_verbose = True
 		else:
 			print("ERROR:parameter error !!! arg[%d]:%s" % (idx,arg[idx]))
 			return
@@ -195,21 +229,33 @@ def get_process(arg):
 			item_start = False
 
 		if item_start == True:
-			if interval_flag == False:
-				print("\n<%s>m:%s f:%-20s|" % (field[0],field[4],field[2][-20:]),end="")
-			else:
-				ticks_cur = int(field[0]);
-				if ticks_prv == 0:
+			if pr_noticks == False:
+				if interval_flag == False:
+					print("\n<%s>" % field[0],end="")
+				else:
+					ticks_cur = int(field[0]);
+					if ticks_prv == 0:
+						ticks_prv = ticks_cur
+					interval_time = (ticks_cur - ticks_prv)*1000000/cpu_frequence
+					print("\n[%6dus]<%s>" % (interval_time,field[0]),end="")
 					ticks_prv = ticks_cur
-				interval_time = (ticks_cur - ticks_prv)*1000000/cpu_frequence
-				print("\n[%6dus]<%s>m:%s f:%-20s|" % (interval_time,field[0],field[4],field[2][-20:]),end="")
-				ticks_prv = ticks_cur
+			else:
+				print("")
+			if pr_nomagic == False:
+				print("m:%s" % field[4],end="")
+			if pr_nofunc == False:
+				print(" f:%-20s" % field[2][-20:],end="")
+			print("|",end="")
 			item_pr = item_pr + 1
 		elif item_pr < 7 and item_pr > 0:
-			print("%15s:%-10s|" % (field[2][-15:],field[3]),end="")
-			item_pr = item_pr + 1
+			if pr_verbose == False:
+				print("%15s:%-10s|" % (field[2][-15:],field[3]),end="")
+				item_pr = item_pr + 1
+			else:
+				print(" %s:%s |" % (field[2][-15:],field[3]),end="")
 
 	print("")
+	file.close()
 	return
 
 def cmd_parse(cmd):
@@ -234,9 +280,24 @@ def cmd_parse(cmd):
 		get_process(field)
 	elif field[0] == 'h':
 		print("Usage:")
-		print("dma [-n] [count]")
+		print("dma [-n] [count] [-s] [-nod]")
+		print("    [-s]   select slot to display")
+		print("    [-nod] don't display slot data")
 		print("loop [-n] [count] [-t]")
-		print("get -m magic_word [-n] [count] [-t]")
+		print("get -m magic_word [-n] [count] [-t] [-not] [-nom] [-nof]")
+		print("    [-t]   display interval")
+		print("    [-not] don't display interval")
+		print("    [-nom] don't display magic world")
+		print("    [-nof] don't display function")
+		print("\r\n----- basic magic world ------")
+		print("loop start         :0x64720001")
+		print("loop end           :0x6472000B")
+		print("FhTxValid          :0x64720003")
+		print("FhRxValid          :0x64721010")
+		print("cpa_fh_rx_callback :0xBCBCBCBC")
+		print("bbupool_onetask_gen:0x10203040")
+		print("task_dl_config     :0xCCBBCCBB")
+		print("------------------------------\r\n")
 
 	return
 
